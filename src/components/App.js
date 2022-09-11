f913bf4f0ee1aa2e9b19744b6be362f6c70d740c
Pranjay91 <pranjaylohbare91@gmail.com>
2022-09-11 15:36:57 +0530

    .

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+      fdbhqe9-thb
+    </div>
+  )
+}
+
+
+export default App;
